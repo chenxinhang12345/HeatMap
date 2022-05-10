b730759954e938b462fedf5c43a7c3f46be6f00a
Xinhang Chen <xinhang2@illinois.edu>
2022-05-10 03:23:36 -0500

    add basic map frontend

---
+++ map/components/map.tsx
@@ -0,0 +1,41 @@
+import { useState, useMemo, useCallback, useRef } from "react";
+import {
+  GoogleMap,
+} from "@react-google-maps/api";
+
+type LatLngLiteral = google.maps.LatLngLiteral;
+type MapOptions = google.maps.MapOptions;
+
+export default function Map() {
+  const mapRef = useRef<GoogleMap>();
+  const center = useMemo<LatLngLiteral>(
+    () => ({ lat: 43.45, lng: -80.49 }),
+    []
+  );
+  const options = useMemo<MapOptions>(
+    () => ({
+      disableDefaultUI: true,
+      clickableIcons: false,
+    }),
+    []
+  );
+  const onLoad = useCallback((map) => (mapRef.current = map), []);
+  return (
+    <div className="container">
+      <div className="controls">
+        <h1>Map</h1>
+      </div>
+      <div className="map">
+        <GoogleMap
+          zoom={10}
+          center={center}
+          mapContainerClassName="map-container"
+          options={options}
+          onLoad={onLoad}
+        >
+        </GoogleMap>
+      </div>
+    </div>
+  );
+}
+
